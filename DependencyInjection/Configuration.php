29acda72d8ec5d8b355877b8803819b3e003a6ec
phiamo <phiamo@gmail.com>
2020-11-26 09:21:44 +0100

    more

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -12,8 +12,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('addressable_bundle');
+        $treeBuilder = new TreeBuilder('addressable_bundle');
+        $rootNode = $treeBuilder->getRootNode();
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
